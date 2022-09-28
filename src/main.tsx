87ed83a72d97c5137bc813f3f1baf1a2408e7137
Juan Carlos Cubillos Gacha <Juan.cubillos@sinco.com.co>
2022-09-28 10:37:23 -0500

    :JK React router

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { GestionProveedoresPage } from './Sitio/Web/Pages/GestionProveedores/GestionProveedoresPage'
-import LoginPages from './Sitio/Web/Pages/GestionProveedores/Login/Pages/LoginPages'
+import LoginPages from './Sitio/Web/Pages/Login/Pages/LoginPages'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
