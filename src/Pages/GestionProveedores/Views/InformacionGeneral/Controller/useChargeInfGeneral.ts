352bba623467322a12d620a2b51af65582b4e249
Juan Carlos Cubillos Gacha <Juan.cubillos@sinco.com.co>
2022-10-04 08:58:49 -0500

    :JK

--- src/Pages/GestionProveedores/Views/InformacionGeneral/Controller/useChargeInfGeneral.ts
+++ src/Pages/GestionProveedores/Views/InformacionGeneral/Controller/useChargeInfGeneral.ts
@@ -1,11 +1,11 @@
-import React from 'react'
+// import React from 'react'
 
-export const useChargeInfGeneral = (initialForm: infGeneralForm) => {
+// export const useChargeInfGeneral = (initialForm: infGeneralForm) => {
 
-    return{
+//     return{
 
-    }
-}
+//     }
+// }
 
 
 
