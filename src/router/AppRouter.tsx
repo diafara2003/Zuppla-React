25bb192cf2b27c8e4444beacf42cdf152ce51987
jhonnatan uruena <jhonnatan.uruena@sinco.com.co>
2022-10-04 08:58:23 -0500

    :f

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,26 +1,26 @@
 
-import { Route, Routes, } from 'react-router-dom';
+import { Route, Routes,  } from 'react-router-dom';
 
 import LoginPages from '../Pages/Login/views/LoginPages';
+import { MenuPages } from '../Pages/Menu/views/MenuPages';
+import { AuthPages } from '../Auth';
+import { GestionProveedoresPage } from '../Pages/GestionProveedores/';
 import { ProtectedRoutes } from './ProtectedRoutes';
 import { HomePages } from '../Pages/Home/views/HomePages';
-import { routes } from './routes';
 
 
 export const AppRouter = () => {
     return (
         <>
-
-
             <Routes>
 
                 <Route path="/login" element={<LoginPages />}></Route>
 
+
+
                 <Route element={<ProtectedRoutes />}>
 
-                   
-
-                    {
+                {/* {
                         routes.map(({ path, Component: Component }) => {
                             return (
                                 <Route
@@ -29,8 +29,13 @@ export const AppRouter = () => {
                                     element={<Component />} />
                             );
                         })
-                    }
+                    } */}
+                    <Route path="/" element={<HomePages />}></Route>
+                    <Route path="/home" element={<HomePages />}></Route>
 
+
+
+                    <Route path="/gestionproveedor/*" element={<GestionProveedoresPage />} />
                 </Route>
 
             </Routes>
