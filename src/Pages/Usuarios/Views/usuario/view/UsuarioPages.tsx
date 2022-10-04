8dd50bf8f2689fba9bd6c3a760e3bab12ced1214
jhonnatan uruena <jhonnatan.uruena@sinco.com.co>
2022-10-04 17:45:50 -0500

    :f

--- src/Pages/Usuarios/Views/usuario/view/UsuarioPages.tsx
+++ src/Pages/Usuarios/Views/usuario/view/UsuarioPages.tsx
@@ -2,101 +2,18 @@ import { Box, Button, InputAdornment, Paper, Table, TableBody, TableCell, TableC
 import React from 'react'
 import { HeaderComponent } from '../../../../../SharedComponents/Header'
 import SearchIcon from '@mui/icons-material/Search';
-import { Add, MasksRounded } from '@mui/icons-material';
+import { Add, MasksRounded, MoreVert } from '@mui/icons-material';
 import HistoryIcon from '@mui/icons-material/History';
+import { useUsuario } from '../hook/useUsuario';
 export const UsuarioPages = () => {
-    interface Column {
-        id: 'name' | 'code' | 'population' | 'size' | 'density';
-        label: string;
-        minWidth?: number;
-        align?: 'right';
-        format?: (value: number) => string;
-    }
 
-    const columns: readonly Column[] = [
-        { id: 'name', label: 'Name', minWidth: 170 },
-        { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
-        {
-            id: 'population',
-            label: 'Population',
-            minWidth: 170,
-            align: 'right',
-            format: (value: number) => value.toLocaleString('en-US'),
-        },
-        {
-            id: 'size',
-            label: 'Size\u00a0(km\u00b2)',
-            minWidth: 170,
-            align: 'right',
-            format: (value: number) => value.toLocaleString('en-US'),
-        },
-        {
-            id: 'density',
-            label: 'Density',
-            minWidth: 170,
-            align: 'right',
-            format: (value: number) => value.toFixed(2),
-        },
-    ];
-
-    interface Data {
-        name: string;
-        code: string;
-        population: number;
-        size: number;
-        density: number;
-    }
-
-    function createData(
-        name: string,
-        code: string,
-        population: number,
-        size: number,
-    ): Data {
-        const density = population / size;
-        return { name, code, population, size, density };
-    }
-
-    const rows = [
-        createData('India', 'IN', 1324171354, 3287263),
-        createData('China', 'CN', 1403500365, 9596961),
-        createData('Italy', 'IT', 60483973, 301340),
-        createData('United States', 'US', 327167434, 9833520),
-        createData('Canada', 'CA', 37602103, 9984670),
-        createData('Australia', 'AU', 25475400, 7692024),
-        createData('Germany', 'DE', 83019200, 357578),
-        createData('Ireland', 'IE', 4857000, 70273),
-        createData('Mexico', 'MX', 126577691, 1972550),
-        createData('Japan', 'JP', 126317000, 377973),
-        createData('France', 'FR', 67022000, 640679),
-        createData('United Kingdom', 'GB', 67545757, 242495),
-        createData('Russia', 'RU', 146793744, 17098246),
-        createData('Nigeria', 'NG', 200962417, 923768),
-        createData('Brazil', 'BR', 210147125, 8515767),
-    ];
-
-    const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(10);
-
-    const handleChangePage = (event: unknown, newPage: number) => {
-        setPage(newPage);
-    };
-
-    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setRowsPerPage(+event.target.value);
-        setPage(0);
-    };
-
+    const { data, isLoading } = useUsuario();
     return (
 
         <>
             <HeaderComponent title={"Usuarios"} />
 
             <Box>
-
-
-
-
 
                 <Box display={"flex"} justifyContent={"end"} pt={"10px"}>
 
@@ -124,40 +41,93 @@ export const UsuarioPages = () => {
                             <Table stickyHeader aria-label="sticky table" size="small">
                                 <TableHead>
                                     <TableRow>
-                                        {columns.map((column) => (
-                                            <TableCell
-                                                key={column.id}
-                                                align={column.align}
-                                                style={{ minWidth: column.minWidth, fontWeight:'bold' }}
-                                            >
-                                                {column.label}
-                                            </TableCell>
-                                        ))}
+                                        <TableCell
+                                            key={"thNombre"}
+                                            align={"left"}
+                                            style={{ fontWeight: 'bold' }}
+                                        >
+                                            {"Nombre"} </TableCell>
+                                        <TableCell
+                                            key={"thDocumento"}
+                                            align={"left"}
+                                            style={{ fontWeight: 'bold' }}
+                                        >
+                                            {"Documento"}
+                                        </TableCell>
+                                        <TableCell
+                                            key={"thcargo"}
+                                            align={"left"}
+                                            style={{ fontWeight: 'bold' }}
+                                        >
+                                            {"Cargo"}
+                                        </TableCell>
+
+                                        <TableCell
+                                            key={"thcorreo"}
+                                            align={"left"}
+                                            style={{ fontWeight: 'bold' }}
+                                        >
+                                            {"Correo"}
+                                        </TableCell>
+
+                                        <TableCell
+                                            key={"thCelular"}
+                                            align={"left"}
+                                            style={{ fontWeight: 'bold' }}
+                                        >
+                                            {"Celular"}
+                                        </TableCell>
+                                        <TableCell
+                                            key={"thEstado"}
+                                            align={"left"}
+                                            style={{ fontWeight: 'bold' }}
+                                        >
+                                            {"Estado"}
+                                        </TableCell>
+                                        <TableCell
+                                            key={"thAcciones"}
+                                            align={"center"}
+                                            style={{ fontWeight: 'bold' }}
+                                        >
+                                            {"Acciones"}
+                                        </TableCell>
+
                                     </TableRow>
                                 </TableHead>
                                 <TableBody>
-                                    {rows
-                                        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                    {data == null ? null : data
+                                        //.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                                         .map((row) => {
                                             return (
-                                                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                                                    {columns.map((column) => {
-                                                        const value = row[column.id];
-                                                        return (
-                                                            <TableCell key={column.id} align={column.align}>
-                                                                {column.format && typeof value === 'number'
-                                                                    ? column.format(value)
-                                                                    : value}
-                                                            </TableCell>
-                                                        );
-                                                    })}
+                                                <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                                                    <TableCell key={row.id * 2}>
+                                                        {row.nombre}
+                                                    </TableCell>
+                                                    <TableCell key={row.id * 2}>
+                                                        {row.documento}
+                                                    </TableCell>
+                                                    <TableCell key={row.id * 2}>
+                                                        {row.cargo}
+                                                    </TableCell>
+                                                    <TableCell key={row.id * 2}>
+                                                        {row.correo}
+                                                    </TableCell>
+                                                    <TableCell key={row.id * 2}>
+                                                        {row.celular}
+                                                    </TableCell>
+                                                    <TableCell key={row.id * 2}>
+                                                        {row.estado}
+                                                    </TableCell>
+                                                    <TableCell key={row.id * 2} align="center">
+                                                        <MoreVert />
+                                                    </TableCell>
                                                 </TableRow>
                                             );
                                         })}
                                 </TableBody>
                             </Table>
                         </TableContainer>
-                        <TablePagination
+                        {/* <TablePagination
                             rowsPerPageOptions={[10, 25, 100]}
                             component="div"
                             count={rows.length}
@@ -165,7 +135,7 @@ export const UsuarioPages = () => {
                             page={page}
                             onPageChange={handleChangePage}
                             onRowsPerPageChange={handleChangeRowsPerPage}
-                        />
+                        /> */}
                     </Paper>
                 </Box>
 
