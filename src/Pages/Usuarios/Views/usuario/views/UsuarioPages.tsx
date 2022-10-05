543e9374c8cc803e9c8ab139e6c94481673fa953
jhonnatan uruena <jhonnatan.uruena@sinco.com.co>
2022-10-04 21:37:15 -0500

    :d

---
+++ src/Pages/Usuarios/Views/usuario/views/UsuarioPages.tsx
@@ -0,0 +1,47 @@
+import { Box, Button, CircularProgress, InputAdornment, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, TextField } from '@mui/material'
+import React from 'react'
+import { HeaderComponent } from '../../../../../SharedComponents/Header'
+import SearchIcon from '@mui/icons-material/Search';
+import { Add, MasksRounded, MoreVert } from '@mui/icons-material';
+import HistoryIcon from '@mui/icons-material/History';
+import { useUsuario } from '../hook/useUsuario';
+import LstUsuarios from './ListadoUsuarios/view/LstUsuarios';
+export const UsuarioPages = () => {
+
+    const { data, isLoading } = useUsuario();
+    return (
+
+        <>
+            <HeaderComponent title={"Usuarios"} />
+
+            <Box>
+
+                <Box display={"flex"} justifyContent={"end"} pt={"10px"}>
+
+                    <TextField
+                        id="outlined-basic"
+                        size='small'
+                        placeholder='Buscar...'
+                        sx={{ width: "400px" }}
+                        InputProps={{
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <SearchIcon />
+                                </InputAdornment>
+                            ),
+                        }}
+                        label="Buscar..." variant="outlined" />
+                    <Button sx={{ ml: "20px" }} variant="text" > <Add sx={{ mr: "8px" }} />Agregar usuario</Button>
+                    <Button variant="text" > <HistoryIcon sx={{ mr: "8px" }} />Historial</Button>
+                </Box>
+
+
+                <Box m={"10px"} mt={"25px"}>
+                    {data==null ? <CircularProgress color="inherit" /> : <LstUsuarios datatable={data!}/>}
+                  
+                </Box>
+
+            </Box>
+        </>
+    )
+}
