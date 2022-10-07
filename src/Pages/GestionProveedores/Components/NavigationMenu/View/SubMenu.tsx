856da124eae4fbb07f736b79dc5064374f1359b1
jhonnatan uruena <jhonnatan.uruena@sinco.com.co>
2022-10-07 12:15:51 -0500

    :cambio de logo

--- src/Pages/GestionProveedores/Components/NavigationMenu/View/SubMenu.tsx
+++ src/Pages/GestionProveedores/Components/NavigationMenu/View/SubMenu.tsx
@@ -10,7 +10,7 @@ import React from 'react';
 import { LogoEmpresa } from '../../LogoProveedor/View/LogoEmpresa';
 import { BrowserRouter, Navigate, NavLink, Route, Routes } from 'react-router-dom';
 
-import '../Styles/Navigation.css'
+
 
 
 
