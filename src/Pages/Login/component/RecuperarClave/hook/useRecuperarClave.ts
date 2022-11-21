aeb08c66d9418334b5ec0220f58888bfcc712330
jhonnatan uruena <jhonnatan.uruena@sinco.com.co>
2022-11-21 15:05:15 -0500

    :f

--- src/Pages/Login/component/RecuperarClave/hook/useRecuperarClave.ts
+++ src/Pages/Login/component/RecuperarClave/hook/useRecuperarClave.ts
@@ -1,4 +1,91 @@
+
+import { useState, useEffect, useContext } from 'react';
+import { APiMethod, ResponseDTO, useFetch } from '../../../../../Provider';
+import { AlertContext } from '../../../../Menu/context/AlertContext';
+
+
+type validacionFormulario = {
+    email: inputFormulario
+
+}
+
+
+type inputFormulario = {
+    hasError: boolean, msn: string
+}
 export const useRecuperarClave = () => {
 
-    return {}
+
+    const [showAlert, stateAlert] = useState(false);
+    const { hasError, data, isLoading, doFetch } = useFetch<ResponseDTO | null>();
+    const [stateCorreo, setStateCorreo] = useState("");
+    const [errorState, errorStateState] = useState<validacionFormulario>(
+        {
+            email: { hasError: false, msn: '' },
+
+        }
+
+    );
+
+    const validarcorreo = (): validacionFormulario => {
+        let result: validacionFormulario = {
+            email: { hasError: false, msn: '' }
+
+        };
+
+        if (stateCorreo == "")
+            return {
+                email: { hasError: true, msn: 'Correo obligatorio' },
+            }
+
+        const emailRegexp = new RegExp(/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/);
+        const hasError = !emailRegexp.test(stateCorreo);
+
+
+        if (hasError)
+            return {
+                email: { hasError: true, msn: 'Correo ingresado invalido' },
+            }
+
+
+        return result;
+    }
+
+    const handleRecordarClave = (e: React.SyntheticEvent) => {
+
+        e.preventDefault();
+
+        const _validacion = validarcorreo();
+
+        if (_validacion.email.hasError) {
+            errorStateState(_validacion);
+            return;
+        }
+
+
+        doFetch({
+            metodo: 'Account/recordarclave',
+            type: APiMethod.POST,
+            data: {
+                usuario: stateCorreo
+            }
+        });
+    }
+
+    useEffect(() => {
+
+        if (data != null) {
+
+            stateAlert(true);
+            setStateCorreo("");
+        }
+    }, [data])
+
+
+    const onInputChange = (e: React.SyntheticEvent) => {
+        const { value } = (e.target as HTMLInputElement);
+        setStateCorreo(value);
+    }
+
+    return { isLoading, handleRecordarClave, stateCorreo, onInputChange, ...errorState, showAlert, stateAlert }
 }
--- src/Pages/Login/component/RecuperarClave/view/RecuperarClavepages.tsx
+++ src/Pages/Login/component/RecuperarClave/view/RecuperarClavepages.tsx
@@ -1,13 +1,15 @@
-import { Avatar, Box, Container, Typography, TextField, Button } from '@mui/material';
+import { Avatar, Box, Container, Typography, TextField, Button, Snackbar } from '@mui/material';
 import VpnKeyOutlinedIcon from '@mui/icons-material/VpnKeyOutlined';
 import { LoadingButton } from '@mui/lab';
 import KeyboardBackspaceOutlinedIcon from '@mui/icons-material/KeyboardBackspaceOutlined';
 import { useNavigate } from 'react-router-dom';
+import { useRecuperarClave } from '../hook/useRecuperarClave';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 
 export const RecuperarClavepages = () => {
 
     const navigate = useNavigate();
-
+    const { isLoading, handleRecordarClave, onInputChange, stateCorreo, email, showAlert, stateAlert } = useRecuperarClave();
     return (
         <Container maxWidth="md" >
             <Box
@@ -24,18 +26,22 @@ export const RecuperarClavepages = () => {
                         fontSize: "3rem"
                     }} />
                 </Avatar>
-                <Typography component="h1" variant="h5" sx={{mt:2}}>
+                <Typography component="h1" variant="h5" sx={{ mt: 2 }}>
                     ¿Olvido la contraseña?
                 </Typography>
                 <Typography variant="body1" sx={{ color: 'rgba(8, 21, 36, 0.38);' }}>
-                    No te preocupes, te enviaremos las instrucciones para cambiar la clave
+                    No te preocupes, te enviaremos las instrucciones para recordar tu clave
                 </Typography>
-                <Box component="form" noValidate sx={{ mt: 5 }} >
+                <Box component="form" onSubmit={handleRecordarClave} noValidate sx={{ mt: 5 }} >
 
                     <TextField
+                        error={email.hasError}
+                        helperText={email.msn}
                         required
                         fullWidth
                         id="email"
+                        onChange={onInputChange}
+                        value={stateCorreo}
                         label="Correo electronico"
                         name="email"
                         autoComplete="email"
@@ -45,17 +51,18 @@ export const RecuperarClavepages = () => {
                         size="large"
                         type="submit"
                         fullWidth
+                        loading={isLoading}
                         sx={{ mt: 3, mb: 2 }}
                         loadingPosition="start"
                         variant="contained"
-
+                        startIcon={<EmailOutlinedIcon />}
 
                     >
-                        Resetear Contraseña
+                        Recordar contraseña
                     </LoadingButton>
                     <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-                        <Button onClick={()=>{
-                              navigate('/login', { replace: true })
+                        <Button onClick={() => {
+                            navigate('/login', { replace: true })
                         }}>
                             <KeyboardBackspaceOutlinedIcon />
                             Regresar
@@ -64,6 +71,16 @@ export const RecuperarClavepages = () => {
 
                 </Box>
             </Box>
+            {showAlert ?
+                <Snackbar
+                    anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+                    open={true}
+                    autoHideDuration={4000}
+                    onClose={() => { stateAlert(false) }}
+                    message="Se envio un correo con las instrucciones para recordar la contraseña"
+                    key={"bottom" + "right"}
+                /> : null
+            }
         </Container>
     )
 }
